4295005980db2d92baa35d197afc72078d47a29f
Danilo Marcucci <Danimarku00@gmail.com>
2023-01-16 16:26:25 +0100

    Inizio scrittura codice

---
+++ esercizio.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define FILE_NAME_CSV "auto.csv"
+using namespace std;
+
+struct s_info{
+    string categoria, marca, modello, colore;
+
+};
+
+
+void menu(){
+    int scelta;
+    while(true){
+        cout << "";
+        cin >> scelta;
+        switch(scelta){
+            case 1:
+            break;
+
+            default:
+
+        }
+    }
+}
+
+int main(){
+
+    menu();
+    return 0;
+}
