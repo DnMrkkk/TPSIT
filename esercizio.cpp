992cb5204141a98571d310ec9c366e425c182511
Danilo Marcucci <Danimarku00@gmail.com>
2023-01-18 20:50:02 +0100

    continuo esercizio

--- esercizio.cpp
+++ esercizio.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include <fstream>
 #define FILE_NAME_CSV "auto.csv"
 using namespace std;
 
 struct s_info{
     string categoria, marca, modello, colore;
+    string giorni[2][7] = {{"lunedi", "martedi", "mercoledi", "giovedi", "venerdi", "sabato", "domenica"}, {"", "", "", "", "", "", ""}};
 
-};
+}parco1[8];
 
 
-void menu(){
+void vedi_macchine(){
+    ifstream fin(FILE_NAME_CSV, ios::in);
+    
+
+
+
+    fin.close();
+}
+
+void prenota_macchina(){
+
+}
+
+short menu(){
     int scelta;
     while(true){
-        cout << "";
+        cout << "1-Vedi macchine\n2- Prenota macchina>>";
         cin >> scelta;
         switch(scelta){
             case 1:
-            break;
-
-            default:
-
+                vedi_macchine();
+                break;
+            case 2: 
+                prenota_macchina();
+            case 3:
+                return 0;
         }
     }
 }
