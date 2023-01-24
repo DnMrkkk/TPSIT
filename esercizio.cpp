3f85309de40268d27e68dd21996ea6798efcdb0e
Danilo Marcucci <Danimarku00@gmail.com>
2023-01-24 20:35:29 +0100

    continuo svolgimento esercizio

--- esercizio.cpp
+++ esercizio.cpp
@@ -26,42 +26,42 @@ void inserimento(){
     int i = 0;
     ifstream fin(FILE_NAME_CSV, ios::in);
     while(!fin.eof()){
-        for(int c = 0; c < 11; c++)
+        for(int c = 1; c <= 11; c++)
         {
             string appoge;
-            fin >> appoge;
+            getline(fin, appoge, ',');
             switch(c){
-                case 0:
+                case 1:
                     parco1[i].categoria = appoge;
                     break;
-                case 1:
+                case 2:
                     parco1[i].marca = appoge;
                     break;
-                case 2:
+                case 3:
                     parco1[i].modello = appoge;
                     break;
-                case 3:
+                case 4:
                     parco1[i].colore = appoge;
                     break;
-                case 4:
+                case 5:
                     parco1[i].giorni[1][0] = appoge;
                     break;
-                case 5:
+                case 6:
                     parco1[i].giorni[1][1] = appoge;
                     break;
-                case 6:
+                case 7:
                     parco1[i].giorni[1][2] = appoge;
                     break;
-                case 7:
+                case 8:
                     parco1[i].giorni[1][3] = appoge;
                     break;
-                case 8:
+                case 9:
                     parco1[i].giorni[1][4] = appoge;
                     break;
-                case 9:
+                case 10:
                     parco1[i].giorni[1][5] = appoge;
                     break;
-                case 10:
+                case 11:
                     parco1[i].giorni[1][6] = appoge;
                     break;
             }
@@ -72,33 +72,108 @@ void inserimento(){
     fin.close();
 }
 
-void prenota_macchina(){                                    //da completare
-    cout << "Quale marca di macchina vuoi prenotare: ";
+void messaggio_errore(){
+    cout << "Giornata gia' affittata!" << endl;
+}
+
+void refresh_file(){
+
+    fstream fout1(FILE_NAME_CSV, ios::out);
+    fout1 << "";
+    fout1.close();
+
+    fstream fout(FILE_NAME_CSV, ios::app);
+
+    for(int i = 0; i < file_size; i++){
+        fout << parco1[i].categoria << "," << parco1[i].marca << "," << parco1[i].modello << "," << parco1[i].colore << ",";
+        for(int c = 0; c < 7; c++){
+            fout << parco1[i].giorni[1][c] << ",";
+        }
+        fout << endl;
+    }
+
+
+}
+
+void prenota_macchina(){                                 
+    cout << "Inserisci marca macchina da affittare: ";
     string marca;
     cin >> marca;
     bool trovato = false;
-    for(int i = 0; i < file_size; i++)
-        if(parco1[i].marca == marca){
+
+
+    for(int i = 0; i < file_size; i++){
+        if(marca == parco1[i].marca){
             trovato = true;
             break;
-        }   
-
+        }
+    }
+    int riga_prenotazione = -1;
     if(trovato){
-        cout << "Quale modello vuoi affittare: " << endl;       
-        for(int i = 0; i < file_size; i++)
-            if(parco1[i].marca == marca)
-                cout << parco1[i].modello;
+        for(int i = 0; i < file_size; i++){
+            if(marca == parco1[i].marca){
+                cout << parco1[i].modello << endl;
+            }
+        }
+
         string modello;
         cin >> modello;
-    }    
+        for(int i = 0; i < file_size; i++){
+            if(modello == parco1[i].modello){
+                cout << "Inserisci giorno da affittare: ";
+                string giorno; 
+                cin >> giorno;
+                if(giorno == "lunedi"){
+                    if(parco1[i].giorni[1][0] == "L")
+                        parco1[i].giorni[1][0] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "martedi"){
+                     if(parco1[i].giorni[1][1] == "L")
+                        parco1[i].giorni[1][1] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "mercoledi"){
+                    if(parco1[i].giorni[1][2] == "L")
+                         parco1[i].giorni[1][2] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "giovedi"){
+                     if(parco1[i].giorni[1][3] == "L")
+                        parco1[i].giorni[1][3] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "venerdi"){
+                    if(parco1[i].giorni[1][4] == "L")
+                        parco1[i].giorni[1][4] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "sabato"){
+                     if(parco1[i].giorni[1][5] == "L")
+                        parco1[i].giorni[1][5] = "A";
+                    else
+                        messaggio_errore();
+                }else if(giorno == "domenica"){
+                    if(parco1[i].giorni[1][6] == "L")
+                        parco1[i].giorni[1][6] = "A";
+                    else
+                        messaggio_errore();
+                }else
+                    cout << "Giornata non riconosciuta" << endl;
+            break;
+            }else
+                cout << "Nessuna macchina trovata!" << endl;
+    
+        }
+    }
+    refresh_file();
 }
 
 short menu(){
-
     inserimento();
     int scelta;
     while(true){
-        cout << "1-Vedi macchine\n2- Prenota macchina\n>>";
+        cout << "1-Vedi macchine\n2- Prenota macchina\n3- Esci\n>> ";
         cin >> scelta;
         switch(scelta){
             case 1:
@@ -106,10 +181,12 @@ short menu(){
                 break;
             case 2: 
                 prenota_macchina();
+                break;
             case 3:
                 return 0;
         }
     }
+    return 0;
 }
 
 int main(){
